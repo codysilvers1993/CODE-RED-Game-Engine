ca079ee405daf3de231d9dd7bd95067909f0b1fe
Cody Allen Silvers <72716420+codysilvers1993@users.noreply.github.com>
2020-11-17 20:57:43 -0700

    Add files via upload

---
+++ BS GAME ENGINE/BS GAME ENGINE/BS GAME ENGINE.cpp
@@ -0,0 +1,30 @@
+#include <windows.h>
+#include <iostream>
+#include <winuser.h>
+
+using namespace std;
+
+void onStartUp() {
+
+	cout << "************Welcome To The Code-Red 3-D Game Engine****************";
+
+};
+
+
+
+
+
+
+void main(void) {
+
+	onStartUp();
+
+
+
+
+
+
+
+
+
+};
